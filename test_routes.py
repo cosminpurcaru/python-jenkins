d3edbbda36c25397a030dcedadfd18ef6c09c9fe
Cosmin Purcaru <cospur@MacBookPro.local>
2023-03-25 00:27:45 +0000

    Add Flask app and Docker file

---
+++ test_routes.py
@@ -0,0 +1,7 @@
+from main import app
+
+
+def test_home():
+    response = app.test_client().get('/')
+    assert b'Welcome to "Implement CI/CD using Jenkins for python application"' in response.data
+    assert response.status_code == 200
